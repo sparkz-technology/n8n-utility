e507aa261e3c40f1e14adb3f22d97f0142be2476
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-06-21 15:53:11 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,4 @@
 export default {
     port: process.env.PORT || 3000,
     apiKey: process.env.API_KEY || 'your-very-secure-default-key',
-    ffmpeg: {
-        width: 1280,
-        height: 720,
-        duration: 5
-    }
 };
